b653dba8f47034fa136f38e0abd6c15a5dce6d36
Aiden Hadisi <so.hadisi@gmail.com>
2023-09-30 07:12:36 -0700

    fix contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,47 +1,37 @@
 <template>
   <div
-    class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"
+    class="relative bg-cover bg-center min-h-[650px] flex items-center px-3 py-5"
+    style="background-image: url('/exterior.webp')"
   >
-    <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-      <div class="relative bg-white shadow rounded-3xl">
-        <div class="max-w-lg mx-auto">
-          <img
-            class="h-42 rounded-t-lg"
-            src="exterior.webp"
-            alt="Church Image"
-          />
-          <div class="flex items-center space-x-5 p-6">
-            <div class="font-display">
-              <h1 class="font-bold text-xl text-indigo-500">Contact Us</h1>
-              <p class="text-gray-500">We would love to hear from you!</p>
-            </div>
+    <div class="absolute inset-0 bg-[#283252] opacity-70" />
+    <div
+      class="relative flex items-stretch rounded-xl bg-gray-100 bg-opacity-70"
+      data-aos="zoom-in-up"
+      data-aos-duration="3000"
+    >
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13263.941103081504!2d-117.8158441!3d33.7868784!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcda16d1cca171%3A0x488f94fc622f304a!2sLife%20Church%20of%20Orange%20CA!5e0!3m2!1sen!2sus!4v1696045226879!5m2!1sen!2sus"
+        width="500"
+        height="400"
+        style="border: 0"
+        loading="lazy"
+        class="max-w-full opacity-80 rounded-l-xl"
+      ></iframe>
+      <div class="p-8 text-gray-600">
+        <h1 class="text-3xl font-display mb-4 text-brown">Contact Us</h1>
+        <div class="flex flex-col space-y-8">
+          <div class="text-center">
+            <Icon class="text-3xl mb-2" name="ph:phone-call-duotone" />
+            <p>(714) 771-2522</p>
+          </div>
+          <div class="text-center">
+            <Icon class="text-3xl mb-2" name="ph:map-pin-line-duotone" />
+            <p>3524 E Chapman Ave<br />Orange, CA 92869</p>
           </div>
 
-          <div class="divide-y divide-gray-200 p-6 pt-0">
-            <div
-              class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
-            >
-              <p>
-                <strong>Address:</strong> 3524 E Chapman Ave Orange, CA 92869
-              </p>
-              <p><strong>Phone:</strong> (714) 771-2522</p>
-              <p><strong>Email:</strong> info@lifeoc.org</p>
-            </div>
-            <div
-              class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7"
-            >
-              <h1 class="font-bold text-xl text-indigo-500 font-display mb-4">
-                Find us on Google Maps
-              </h1>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13263.941103081504!2d-117.8158441!3d33.7868784!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcda16d1cca171%3A0x488f94fc622f304a!2sLife%20Church%20of%20Orange%20CA!5e0!3m2!1sen!2sus!4v1696045226879!5m2!1sen!2sus"
-                width="600"
-                height="450"
-                style="border: 0"
-                loading="lazy"
-                class="max-w-full"
-              ></iframe>
-            </div>
+          <div class="text-center">
+            <Icon class="text-3xl mb-2" name="ph:envelope-duotone" />
+            <p>info@lifeoc.org</p>
           </div>
         </div>
       </div>
